0fb1a60fc4e280e8af3294d77b63e606fa3c1cdc
elfer07 <morenofernando@gmail.com>
2022-02-27 13:25:23 -0300

    ViewBinding

--- app/src/main/java/ar/com/touchzoomimageandroid/MainActivity.kt
+++ app/src/main/java/ar/com/touchzoomimageandroid/MainActivity.kt
@@ -2,10 +2,16 @@ package ar.com.touchzoomimageandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ar.com.touchzoomimageandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
